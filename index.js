6caf4372d6b48fde309595d3a54acb6d11ea6b9b
Cássia Oliveira dos Santos <86209186+CassiaSantos@users.noreply.github.com>
2024-09-13 20:15:17 -0300

    feat: implemeta cadastro e listagem parcial das metas

--- index.js
+++ index.js
@@ -1,12 +1,77 @@
-const start = () => {
+const {select, input} = require('@inquirer/prompts')
+
+// Lista de metas:
+let meta = {
+    value: "Fazer musculação 2 horas por dia",
+    checked: false
+}
+
+let metasList = [meta]
+
+// CADASTRO DE METAS - Aguarda usuário digitar meta no console:
+const cadastrarMeta = async () => {
+    const meta = await input ({message: "Digite a meta: "})
+
+    if(meta.length == 0) {
+        console.log("A meta não pode ser vazia")
+        return
+    }
+    metasList.push({
+        value: meta,
+        checked: false
+    })
+}
+
+// LISTAGEM DE METAS: 
+const listarMeta = async () => {
+    const repostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metasList] //faz uma cópia de metasList para que a original não seja modificada;
+    })
+
+    if(respostas.length == 0) {
+        console.log("Nenhuma meta selecionada")
+        return
+    }
+
+    respostas.forEach((resposta) => {
+        const meta = metasList.find(m) => {
+            return m.value == resposta
+        })
+    }) 
+}
+
+const start = async () => {
+
     while(true){
-        let opcao = "sair"
+
+        // Menu de opções + poder de escolha do usuário:
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar Meta",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        // A opção escolhida pelo usuário é passada e analisada
         switch(opcao) {
             case "cadastrar":
                 console.log("Vamos cadastrar")
+                await cadastrarMeta()
                 break
             case "listar":
-                console.log("Vamos listar")
+                listarMeta()
                 break
             case "sair":
                 return
